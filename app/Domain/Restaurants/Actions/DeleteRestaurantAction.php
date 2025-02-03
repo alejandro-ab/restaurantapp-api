d8574a43f08348862c060c2a3cdcadcae1b05a3c
Alejandro Alvarez Botero <aalvarezbo@unal.edu.co>
2025-02-02 19:45:45 -0500

    feat: delete images when a restaurant in deleted

--- app/Domain/Restaurants/Actions/DeleteRestaurantAction.php
+++ app/Domain/Restaurants/Actions/DeleteRestaurantAction.php
@@ -2,12 +2,16 @@
 
 namespace App\Domain\Restaurants\Actions;
 
+use App\Domain\Images\Actions\DeleteImageAction;
+use App\Models\Image;
 use App\Models\Restaurant;
 
 class DeleteRestaurantAction
 {
     public static function execute(Restaurant $restaurant): void
     {
+        $restaurant->images()->each(fn(Image $image) => DeleteImageAction::execute($image));
+
         $restaurant->delete();
     }
 }
