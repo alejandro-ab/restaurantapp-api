75df7b9fe15302d9d037f7d3578f6ee97f77f3c5
LuisVarel4 <luvarelao@unal.edu.co>
2025-02-09 23:17:45 -0500

    create tag route

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,7 @@ Route::get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->group(function () {
     Route::resource('/restaurants', RestaurantController::class)->except(['create', 'edit']);
 
+    Route::resource('/tags', RestaurantController::class)->except(['create', 'edit']);
+
     Route::resource('/images', ImageController::class)->only(['show', 'store', 'destroy']);
 });
