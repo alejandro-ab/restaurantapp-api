370de3bb08f26bcf94bfc1e606634792aab9c9da
Alejandro Alvarez Botero <aalvarezbo@unal.edu.co>
2025-02-16 20:54:56 -0500

    refactor: refactor delete

--- app/Domain/Visits/Actions/DeleteVisitAction.php
+++ app/Domain/Visits/Actions/DeleteVisitAction.php
@@ -2,12 +2,16 @@
 
 namespace App\Domain\Visits\Actions;
 
+use App\Domain\Images\Actions\DeleteImageAction;
+use App\Models\Image;
 use App\Models\Visit;
 
 class DeleteVisitAction
 {
     public static function execute(Visit $visit): void
     {
+        $visit->images()->each(fn(Image $image) => DeleteImageAction::execute($image));
+
         $visit->delete();
     }
 }
