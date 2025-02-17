ee210664d3e5eb66b260b009569b1cc5bc137276
Alejandro Alvarez Botero <aalvarezbo@unal.edu.co>
2025-02-16 21:06:25 -0500

    feat: delete dishes and visits when a restaurant is deleted

--- app/Domain/Restaurants/Actions/DeleteRestaurantAction.php
+++ app/Domain/Restaurants/Actions/DeleteRestaurantAction.php
@@ -2,15 +2,21 @@
 
 namespace App\Domain\Restaurants\Actions;
 
+use App\Domain\Dishes\Actions\DeleteDishAction;
 use App\Domain\Images\Actions\DeleteImageAction;
+use App\Domain\Visits\Actions\DeleteVisitAction;
+use App\Models\Dish;
 use App\Models\Image;
 use App\Models\Restaurant;
+use App\Models\Visit;
 
 class DeleteRestaurantAction
 {
     public static function execute(Restaurant $restaurant): void
     {
         $restaurant->images()->each(fn(Image $image) => DeleteImageAction::execute($image));
+        $restaurant->visits()->each(fn(Visit $visit) => DeleteVisitAction::execute($visit));
+        $restaurant->dishes()->each(fn(Dish $dish) => DeleteDishAction::execute($dish));
 
         $restaurant->delete();
     }
