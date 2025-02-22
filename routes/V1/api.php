0d1c15154ffd6209ebace5d55e367490699f3889
Alejandro Alvarez Botero <aalvarezbo@unal.edu.co>
2025-02-21 22:52:42 -0500

    feat: add logout

--- routes/V1/api.php
+++ routes/V1/api.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout'])->name('logout');
 
     Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->name('password.forgot');
     Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
